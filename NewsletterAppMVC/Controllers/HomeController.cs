70a368594002219402281ad7ac25b116f166c46d
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-19 07:52:41 +0100

    Implement logic if an error occures

--- NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/Controllers/HomeController.cs
@@ -12,7 +12,10 @@ namespace NewsletterAppMVC.Controllers
         [HttpPost]
         public ActionResult SignUp(string firstName, string lastName, string emailAddress) //the model biding will bind the info passsed from the form, they have to have the exact name - not necesarly first letter uppercase
         {
-            return null;
+            if (string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
         }
 
         public ActionResult About()
