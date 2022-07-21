4155362bde673579bf8958483c2762e5cb566381
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-21 12:16:43 +0100

    Add ViewModels signupVm class

---
+++ NewsletterAppMVC/ViewModels/SignupVm.cs
@@ -0,0 +1,9 @@
+﻿namespace NewsletterAppMVC.ViewModels
+{
+    public class SignupVm // best practice to end the View Models classes with Vm
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
