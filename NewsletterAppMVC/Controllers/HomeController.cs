90e4418143d7a4d191c969cf5d452281591af107
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-21 10:39:05 +0100

    Remove about and contact controller and views

--- NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/Controllers/HomeController.cs
@@ -45,18 +45,7 @@ namespace NewsletterAppMVC.Controllers
             }
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
