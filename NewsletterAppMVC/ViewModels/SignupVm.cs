fc3ae29a21a042a55dec25d11c363b58ecdce948
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-21 17:31:04 +0100

    Add Id prop to the signup view model

--- NewsletterAppMVC/ViewModels/SignupVm.cs
+++ NewsletterAppMVC/ViewModels/SignupVm.cs
@@ -2,6 +2,7 @@
 {
     public class SignupVm // best practice to end the View Models classes with Vm
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
