74b3448e9ef9cd9ca1368281cdfa4787bf0a9d58
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-21 16:43:05 +0100

    Remove unwanted code

--- NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/Controllers/HomeController.cs
@@ -67,32 +67,6 @@ namespace NewsletterAppMVC.Controllers
 
                 return View(signupVms);
             }
-
-
-            //string queryString = @"SELECT Id, FirstName, LastName, EmailAddress , SocialSecurityNumber from SignUps";
-            //List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
-
-            //using (SqlConnection connection = new SqlConnection(conectionString))
-            //{
-            //    SqlCommand command = new SqlCommand(queryString, connection);
-            //    connection.Open();
-
-            //    SqlDataReader reader = command.ExecuteReader();
-
-            //    while (reader.Read())
-            //    {
-            //        var signup = new NewsletterSignUp();
-            //        signup.Id = Convert.ToInt32(reader["Id"]);
-            //        signup.FirstName = reader["FirstName"].ToString();
-            //        signup.LastName = reader["LastName"].ToString();
-            //        signup.EmailAddress = reader["EmailAddress"].ToString();
-            //        signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
-
-            //        signups.Add(signup);
-            //    }
-            //}
-
-
         }
 
     }
