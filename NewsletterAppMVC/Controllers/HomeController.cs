4ec76c73396b02a9ae0629052dfca56893799a32
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-21 17:21:28 +0100

    Remove Swl connection string

--- NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/Controllers/HomeController.cs
@@ -4,8 +4,6 @@ namespace NewsletterAppMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly string conectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
         public ActionResult Index()
         {
             return View();
