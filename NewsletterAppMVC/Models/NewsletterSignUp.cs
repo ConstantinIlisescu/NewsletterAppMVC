a38a907179610653b6c43430488e55e3d1b87609
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-21 10:32:55 +0100

    Add properties to the model, they are exactly matching the database column titles

--- NewsletterAppMVC/Models/NewsletterSignUp.cs
+++ NewsletterAppMVC/Models/NewsletterSignUp.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace NewsletterAppMVC.Models
+﻿namespace NewsletterAppMVC.Models
 {
     public class NewsletterSignUp
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
     }
 }
